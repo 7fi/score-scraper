429f6e1619ecba2be5543bc7f4c9c860872160af
7fi <carterwilliamanderson@gmail.com>
2022-12-04 16:30:32 -0800

    more cleanup, started on restructuring

--- scrapeGraph.py
+++ scrapeGraph.py
@@ -6,16 +6,23 @@ from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 from scipy.interpolate import make_interp_spline, BSpline
 
+# "SSP gold": "s22/nwisa-combined-division", "SSP silver": "s22/nwisa-combined-division-silver", after oak # "bellingham": "s22/bellingham-fleet-race/", after bronze, 
+# regattas = {"south regional": "s22/south-regional-nwisa",  "oak gold": "s22/island-cup", 
+#             "oak silver": "s22/islands-cup-silver", "SSP bronze":"s22/nwisa-combined-division-bronze", 
+#             "anacortes gold" :"s22/seafarers-cup-gold", "anacortes silver":"s22/seafarers-cup-silver", 
+#             "fleet champs gold":"s22/nwisa-doublehanded", "fleet champs silver":"s22/nwisa-silver-fleet-champs", 
+#             "PT open":"s22/port-townsend-open"}
 
-regatta_names = ["nwisa-girls-qualifiers", "cascadia-cup-gold", "2022-issa-pcisa-all-girls-invitational",
-                 "pontiac-bay-regional-south-regional", "fall-champs", "2022-atlantic-coast"]
-better_names = ["girls quals", "cascadia gold", "PCISA girls",
-                "south regional", "fall champs", "ACCs"]
-names = {"Elliott Chalcraft": "#e0570d", 'Carter Anderson':"#3684a3", "Ryan Downey": "#2de00d", "Sabrina Anderson": "#d20de0"}
-Type = "points"
+regattas = {"ACCs": "f22/2022-atlantic-coast"}
+# better_spring = [, , "bellingham gold", "bellingham silver", "bellingham bronze"]
 
+# regatta_names = ["f22/nwisa-girls-qualifiers", "f22/cascadia-cup-gold", "f22/2022-issa-pcisa-all-girls-invitational",
+#                  "f22/pontiac-bay-regional-south-regional", "f22/fall-champs", "f22/2022-atlantic-coast"]
+# better_names = ["girls quals", "cascadia gold", "PCISA girls",
+#                 "south regional", "fall champs", "ACCs"]
+names = {"Elliott Chalcraft": "#e0570d"} # , 'Carter Anderson':"#3684a3", "Ryan Downey": "#2de00d", "Sabrina Anderson": "#d20de0", "Barrett Lhamon": "#f00"
+Type = "Points"
 people = []
-
 
 class person:
     def __init__(self, name, team, races):
@@ -68,17 +75,17 @@ def getData(type, name, fleet, division, position, pair, regatta):
             for r in p.races:
                 # print(r.score)
                 if r.venue == regatta:
-                    if type == "raw":
+                    if type == "Raw":
                         if isinstance(r.score, int):
                             data[f"{regatta} {r.division}{r.number}"] = r.score
                         else:
                             data[f"{regatta} {r.division}{r.number}"] = len(r.teams) + 1
-                    if type == "points":
+                    if type == "Points":
                         if isinstance(r.score, int):
                             data[f"{regatta} {r.division}{r.number}"] = len(r.teams) - r.score + 1
                         else:
                             data[f"{regatta} {r.division}{r.number}"] = len(r.teams) + 1
-                    if type == "ratio":
+                    if type == "Ratio":
                         if isinstance(r.score, int):
                             data[f"{regatta} {r.division}{r.number}"] = 1 - (r.score / len(r.teams))
                         else:
@@ -97,26 +104,26 @@ def compare(first, second):
     else:
         return 0
 
-for i, regatta in enumerate(regatta_names):
-    betterVenue = better_names[i]
-    print(f"({i + 1}/{len(regatta_names)})analyzing {betterVenue}")
+for i, regatta in enumerate(list(regattas.values())):
+    betterVenue = list(regattas.keys())[i]
+    print(f"({i + 1}/{len(list(regattas.values()))}) analyzing {betterVenue}")
     # full scores
-    url = f"https://scores.hssailing.org/f22/{regatta}/full-scores/"
+    url = f"https://scores.hssailing.org/{regatta}/full-scores/"
     page = requests.get(url)
     fullScores = BeautifulSoup(page.content, 'html.parser')
 
     # sailors a
-    url = f"https://scores.hssailing.org/f22/{regatta}/sailors/"
+    url = f"https://scores.hssailing.org/{regatta}/sailors/"
     page = requests.get(url)
     sailors = BeautifulSoup(page.content, 'html.parser')
 
     # A
-    url = f"https://scores.hssailing.org/f22/{regatta}/A/"
+    url = f"https://scores.hssailing.org/{regatta}/A/"
     page = requests.get(url)
     a = BeautifulSoup(page.content, 'html.parser')
 
     # B
-    url = f"https://scores.hssailing.org/f22/{regatta}/B/"
+    url = f"https://scores.hssailing.org/{regatta}/B/"
     page = requests.get(url)
     b = BeautifulSoup(page.content, 'html.parser')
 
@@ -159,16 +166,33 @@ for i, regatta in enumerate(regatta_names):
         teamBSkippers = []
         teamBCrews = []
 
-        allNames = a.find_all('td', class_="teamname")
+        allNames = sailors.find_all('td', class_="teamname")
+        # print(allNames)
         teamNameEl = [i for i in allNames if i.text == teamName][0]
+        # print(teamNameEl)
 
-        for skipper in teamNameEl.parent.previous_sibling.find_all('td', class_="skipper"):
+
+        rowClass = teamNameEl.parent['class'][1]
+        # print(sailors.find_all("tr", class_=rowClass))
+        # print(teamNameEl.parent.parent.find_all("tr", class_=rowClass))
+        for i,row in enumerate(sailors.find_all("tr", class_=rowClass)):
+            if i != 0 and row['class'][0] == "topborder":
+                break
+            skipper = row.contents[len(row.contents) - 4]
+            print(skipper)
+                
+
+
+        for skipper in teamNameEl.parent.find_all('td', class_="skipper"):
             if skipper.parent.previous_sibling and skipper.parent.previous_sibling.find_all('td', class_="skipper"):
                 skipper2 = skipper.parent.previous_sibling.find(
                     'td', class_="skipper")
                 raceNums = skipper2.next_sibling.text.split(",")
                 raceNums = [i.split("-", 1) for i in raceNums]
                 addPerson(skipper2.text.split(" '")[0],"Skipper", 'A', teamHome, raceNums, teamAScores, teamHomes, betterVenue)
+            # print(skipper)
+            division = skipper.parent.find_all('td', class_="division-cell").text
+            # print(division)
             raceNums = skipper.next_sibling.text.split(",")
             raceNums = [i.split("-", 1) for i in raceNums]
             addPerson(skipper.text.split(" '")[0],"Skipper",'A', teamHome, raceNums, teamAScores, teamHomes, betterVenue)
@@ -190,46 +214,47 @@ for i, regatta in enumerate(regatta_names):
                     races = [i.split("-", 1) for i in races]
                     addPerson(crew3.text.split(" '")[0],"Crew",'A', teamHome, races, teamAScores, teamHomes, betterVenue)
 
-        allNames = b.find_all('td', class_="teamname")
-        teamNameEl = [i for i in allNames if i.text == teamName][0]
+        # allNames = b.find_all('td', class_="teamname")
+        # teamNameEl = [i for i in allNames if i.text == teamName][0]
 
-        for skipper in teamNameEl.parent.previous_sibling.find_all('td', class_="skipper"):
-            if skipper.parent.previous_sibling and skipper.parent.previous_sibling.find_all('td', class_="skipper"):
-                skipper2 = skipper.parent.previous_sibling.find(
-                    'td', class_="skipper")
-                races = skipper2.next_sibling.text.split(",")
-                races = [i.split("-", 1) for i in races]
-                addPerson(skipper2.text.split(" '")[0],"Skipper",'B', teamHome, races, teamBScores, teamHomes, betterVenue)
-            races = skipper.next_sibling.text.split(",")
-            races = [i.split("-", 1) for i in races]
-            addPerson(skipper.text.split(" '")[0],"Skipper",'B', teamHome, races, teamBScores, teamHomes, betterVenue)
+        # for skipper in teamNameEl.parent.previous_sibling.find_all('td', class_="skipper"):
+        #     if skipper.parent.previous_sibling and skipper.parent.previous_sibling.find_all('td', class_="skipper"):
+        #         skipper2 = skipper.parent.previous_sibling.find(
+        #             'td', class_="skipper")
+        #         races = skipper2.next_sibling.text.split(",")
+        #         races = [i.split("-", 1) for i in races]
+        #         addPerson(skipper2.text.split(" '")[0],"Skipper",'B', teamHome, races, teamBScores, teamHomes, betterVenue)
+        #     races = skipper.next_sibling.text.split(",")
+        #     races = [i.split("-", 1) for i in races]
+        #     addPerson(skipper.text.split(" '")[0],"Skipper",'B', teamHome, races, teamBScores, teamHomes, betterVenue)
 
-        for crew in teamNameEl.parent.find_all('td', class_="crew"):
-            races = crew.next_sibling.text.split(",")
-            races = [i.split("-", 1) for i in races]
-            addPerson(crew.text.split(" '")[0],"Crew",'B', teamHome, races, teamBScores, teamHomes, betterVenue)
-            if crew.parent.next_sibling and crew.parent.next_sibling.find_all('td', class_="crew"):
-                crew2 = crew.parent.next_sibling.find(
-                    'td', class_="crew")
-                races = crew2.next_sibling.text.split(",")
-                races = [i.split("-", 1) for i in races]
-                addPerson(crew2.text.split(" '")[0],"Crew",'B', teamHome, races, teamBScores, teamHomes, betterVenue)
+        # for crew in teamNameEl.parent.find_all('td', class_="crew"):
+        #     races = crew.next_sibling.text.split(",")
+        #     races = [i.split("-", 1) for i in races]
+        #     addPerson(crew.text.split(" '")[0],"Crew",'B', teamHome, races, teamBScores, teamHomes, betterVenue)
+        #     if crew.parent.next_sibling and crew.parent.next_sibling.find_all('td', class_="crew"):
+        #         crew2 = crew.parent.next_sibling.find(
+        #             'td', class_="crew")
+        #         races = crew2.next_sibling.text.split(",")
+        #         races = [i.split("-", 1) for i in races]
+        #         addPerson(crew2.text.split(" '")[0],"Crew",'B', teamHome, races, teamBScores, teamHomes, betterVenue)
 
-                if crew2.parent.next_sibling and crew2.parent.next_sibling.find_all('td', class_="crew"):
-                    crew3 = crew2.parent.next_sibling.find(
-                        'td', class_="crew")
-                    races = crew3.next_sibling.text.split(",")
-                    races = [i.split("-", 1) for i in races]
-                    addPerson(crew3.text.split(" '")[0],"Crew",'B', teamHome, races, teamBScores, teamHomes, betterVenue)
+        #         if crew2.parent.next_sibling and crew2.parent.next_sibling.find_all('td', class_="crew"):
+        #             crew3 = crew2.parent.next_sibling.find(
+        #                 'td', class_="crew")
+        #             races = crew3.next_sibling.text.split(",")
+        #             races = [i.split("-", 1) for i in races]
+        #             addPerson(crew3.text.split(" '")[0],"Crew",'B', teamHome, races, teamBScores, teamHomes, betterVenue)
 
 plt.figure(figsize=(20, 5))
+print("Analyzing done, Graphing...")
 
 prev = 0
 xTicks = []
 nameLabels = []
 maxVals = []
 
-for regatta in better_names:
+for regatta in list(regattas.keys()):
     data = {}
     races = []
     for p in list(names.keys()):
@@ -251,7 +276,7 @@ for regatta in better_names:
             plt.scatter(x, y, color=names[p], alpha=0.5, zorder=1) #label=f'{p.split(" ", 1)[0]} {regatta}',
             if p not in nameLabels:
                 nameLabels.append(p)
-            if len(x) > 3 and Type != "ratio":
+            if len(x) > 3 and Type != "Ratio":
                 xnew = np.linspace(min(x), max(x), 300)  
                 spl = make_interp_spline(x, y, k=3)  # type: BSpline
                 ynew = spl(xnew)
@@ -265,8 +290,8 @@ for regatta in better_names:
 
 plt.xlim([-1,len(xTicks)])
 if Type == "Ratio":
-    plt.ylim([0, 1])
     plt.yticks(np.arange(0,1, 0.1))
+    plt.ylim([0, 1])
 else:
     plt.yticks(np.arange(0, max(maxVals) + 1, 2))
     plt.ylim([0, max(maxVals) + 1])
@@ -278,4 +303,4 @@ plt.tight_layout()
 plt.grid(True)
 plt.savefig("fig.png")
 
-plt.show()
+# plt.show() 
